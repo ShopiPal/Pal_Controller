f475daa0a52f773b831c83075bf42fff704bbca0
Itai Peleg <itai.plg@gmail.com>
2022-03-23 19:25:47 +0200

    first_commit

---
+++ pal_controller_pkg/scripts/pal_controller_node.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import rospy
+
+
+#from std_msgs.msg import Int64
+#from std_msgs.msg import Float64
+from std_msgs.msg import Int16
+
+class Controller:
+
+    def __init__(self):
+        self.leftPwmPublisher = rospy.Publisher("/left_motor_pwm",Int16,queue_size=10)
+        #self.rightPwmPublisher = rospy.Publisher("/right_pwm",Int16,queue_size=10)
+        self.leftEncoderSub = rospy.Subscriber("/encoder_left_ticks",Int16,self.leftEncoder_callback)
+        #self.rightEncoderSub = rospy.Subscriber("/encoder_right_ticks",Int16,rightEncoder_callback)
+
+        ## set parameters
+        
+        self.pwm_left_out =  Int16()
+        self.pwm_left_out.data = 200
+
+        self.left_encoder_value = 0
+
+    
+    def publish_pwm(self):
+          
+        #self.pwm_left_out.data =  palPID(.....)
+        #self.pwm_right_out.data = palPID(.....) 
+        
+
+        self.leftPwmPublisher.publish(self.pwm_left_out)
+        #self.rightPwmPublisher.publish(pwm_right_out)
+        
+    def leftEncoder_callback(self,msg):
+        self.left_encoder_value = msg.data
+    
+    def rightEncoder_callback(self,msg):
+        self.right_encoder_value = msg.data
+
+    def stop_robot():
+        return            
+
+if __name__ == '__main__':
+    rospy.init_node('pal_controller_node', anonymous=True)
+    
+    pal_control = Controller()
+       
+    rate = rospy.Rate(10)
+    
+    while not rospy.is_shutdown():
+        pal_control.publish_pwm()
+        
+        rate.sleep()
+
+    
+    
